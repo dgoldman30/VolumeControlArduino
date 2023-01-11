cd486a5189e75111badec9cf5a2362ca0d4d8ee1
Daniel Goldman <dgoldman@vassar.edu>
2023-01-11 01:34:10 -0500

    Finished C# program to actually read from the Serial monitor correctly and now the project is operational, but could use UI.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AudioSwitcher.AudioApi.CoreAudio;
 using System.IO.Ports;
-using java.awt;
+
 
 namespace VolumeControlArduino
 {
@@ -13,16 +13,6 @@ namespace VolumeControlArduino
     {
         static void Main(string[] args)
         {
-            TrayIcon = new NotifyIcon();
-            trayIcon.Text = "My application";
-            trayIcon.Icon = TheIcon
-
-    // Add menu to the tray icon and show it.
-    trayIcon.ContextMenu = trayMenu;
-            trayIcon.Visible = true;
-
-            Visible = false; // Hide form window.
-            ShowInTaskbar = false; // Remove from taskbar.
             SerialPort sp = new SerialPort();
             sp.BaudRate = 9600;
             sp.PortName = "COM7";
@@ -37,50 +27,75 @@ namespace VolumeControlArduino
             }
             catch
             {
-                //sp.Close();
                 Console.WriteLine("Please plug in your Volume Control Module.");
             }
-
+            bool muted = false;
             while (true)
             {
                 if (sp.IsOpen)
                 {
-                    var val = sp.ReadLine().Substring(0);
-                    double value;
-                    
-                    try 
+
+
+                    try
                     {
-                        value = Convert.ToDouble(val);
-                        if (value == -100)
+                        String val = sp.ReadLine().Replace("\r\n", "").Replace("\r", "").Replace("\n", "");
+                        
+                        switch (val)
                         {
-                            
-                            if (microphone.IsMuted == false)
-                            {
-                                microphone.Mute(true);
-                                defaultPlaybackDevice.Mute(true);
-                            }
-         
-                            Console.WriteLine("Muted");
-                        } else
-                        {
-                            if (microphone.IsMuted == true)
-                            {
-                                microphone.Mute(false);
-                                defaultPlaybackDevice.Mute(false);
-                            }
-                            
-                            defaultPlaybackDevice.Volume += value;
-                            Console.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
+                            case "V+":
+                                {
+                                    defaultPlaybackDevice.Volume++;
+                                    break;
+                                }
+                            case "V-":
+                                {
+
+                                    defaultPlaybackDevice.Volume--;
+                                    break;
+                                }
+                            case "M+":
+                                {
+
+                                    microphone.Volume++;
+                                    break;
+                                }
+                            case "M-":
+                                {
+
+                                    microphone.Volume--;
+                                    break;
+                                }
+                            case "MU":
+                                {
+                                    muted = true;
+                                    microphone.Mute(true);
+                                    defaultPlaybackDevice.Mute(true);
+                                    Console.WriteLine("Muted");
+                                    break;
+                                }
+                            case "UN":
+                                {
+                                    muted = false;
+                                    microphone.Mute(false);
+                                    defaultPlaybackDevice.Mute(false);
+                                    Console.WriteLine("Unmuted");
+                                    break;
+
+                                }
                         }
-                       
+                        if (!muted)
+                        {
+                            Console.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume + "-----------------Microphone Volume: " + microphone.Volume);
+                        }
+
                     }
-                    catch (Exception e) {
+                    catch (Exception e)
+                    {
 
                         Console.WriteLine(e);
-                         value = 0;
                     }
-                    
-                    
+
+
                 }
                 else
                 {
